e0fde34315b442f98f2f39650d6ce0590df6f7f2
pkhodakovsky <kravets.taras.react@gmail.com>
2023-11-06 16:21:30 +0200

    added player

--- public/uiStuff.js
+++ public/uiStuff.js
@@ -17,7 +17,16 @@ window.addEventListener('load', () => {
 document.querySelector('.name-form').addEventListener('submit', e => {
   e.preventDefault();
   player.name = document.querySelector('#name-input').value;
+  document.querySelector('.player-name').innerHTML = player.name;
   loginModal.hide();
   spawnModal.show();
   console.log(player);
+});
+
+document.querySelector('.start-game').addEventListener('click', e => {
+  spawnModal.hide();
+  const elArray = Array.from(document.querySelectorAll('.hiddenOnStart'));
+
+  elArray.forEach(el => el.removeAttribute('hidden'));
+  init();
 });
